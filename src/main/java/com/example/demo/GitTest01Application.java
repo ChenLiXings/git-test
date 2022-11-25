931f03d0b70ec15520e1f536af1c850f44c4739c
ChenLiXing <780588029@qq.com>
2022-11-25 15:40:23 +0800

    second commit

--- src/main/java/com/example/demo/GitTest01Application.java
+++ src/main/java/com/example/demo/GitTest01Application.java
@@ -13,5 +13,6 @@ public class GitTest01Application {
     public static void main(String[] args) {
         SpringApplication.run(GitTest01Application.class,args);
         System.out.println("hello Git!");
+        System.out.println("hello World");
     }
 }
