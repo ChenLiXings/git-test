1bef0ecc0797f06d730796d467c7057df05809c4
ChenLiXing <780588029@qq.com>
2023-03-06 10:30:26 +0800

    gitee commit

---
+++ src/main/java/com/example/demo/newclass.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author 陈李星
+ * @create 2023-03-06 10:28
+ */
+public class newclass {
+    public static void main(String[] args) {
+        System.out.println("this is new ONE");
+    }
+}
